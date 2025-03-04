fbdd13900e34a95819cccbdd465bb12347b8ff32
vikram760 <bhosalevikram760.com>
2025-03-04 15:32:50 +0530

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://github.com/VIKRAM9281/Get_Location.git", // Change this to match your GitHub repository name
+  build: {
+    outDir: "docs", // GitHub Pages serves files from the `docs` folder
+  },
+});
