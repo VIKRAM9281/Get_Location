b5ac394aec978b453b9cd059eb3a569c90956bb7
vikram760 <bhosalevikram760.com>
2025-03-04 15:35:35 +0530

    update asset paths for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/VIKRAM9281/Get_Location.git", // Change this to match your GitHub repository name
+  base: "/Get_Location/", // Change this to match your GitHub repository name
   build: {
     outDir: "docs", // GitHub Pages serves files from the `docs` folder
   },
